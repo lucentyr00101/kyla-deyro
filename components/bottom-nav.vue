c0dab82aa8d2fddef27d424e3509ebad5c9b2345
Ranz Sarcos <rsarcos@yondu.com>
2020-12-10 17:00:02 +0800

    added bottom nav footer

---
+++ components/bottom-nav.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  v-bottom-navigation(
+    v-model="value"
+    :background-color="color"
+    dark
+    shift
+    grow
+  )
+    v-btn
+      span Home
+      v-icon mdi-home
+    v-btn
+      span Image
+      v-icon mdi-folder-multiple-image
+    v-btn
+      span Message
+      v-icon mdi-thought-bubble
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: 0
+    }
+  },
+  computed: {
+    color () {
+      switch (this.value) {
+        case 0: return 'blue-grey'
+        case 1: return 'teal'
+        case 2: return 'brown'
+        case 3: return 'indigo'
+        default: return 'blue-grey'
+      }
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,3 @@
 <template lang="pug">
-  div
-    v-parallax(src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" height="300")
-    v-parallax(src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" height="800")
-    v-parallax(src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg" height="500")
+  span Content
 </template>
